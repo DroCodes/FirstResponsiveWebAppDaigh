f60c9622f9c76937d90880f43564437e685599eb
DroCodes <101131393+DroCodes@users.noreply.github.com>
2022-08-31 08:29:11 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstResponsiveWebAppDaigh.Models;
+
+namespace FirstResponsiveWebAppDaigh.Controllers
+{
+    public class HomeController : Controller
+    {
+        const int DEFAULT_VALUE = 0;
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name = "John Doe";
+            ViewBag.Age = DEFAULT_VALUE;
+            ViewBag.YearFormat = "YYYY";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CalculateAgeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Name = model.Name;
+                ViewBag.Age = model.AgeThisYear();
+            }
+            else
+            {
+                ViewBag.Name = "John Doe";
+                ViewBag.Age = DEFAULT_VALUE;
+            }
+            
+            return View();
+        }
+    }
+}
---
+++ Models/CalculateAgeModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppDaigh.Models
+{
+    public class CalculateAgeModel
+    {
+        const int MIN_VALUE = 1900; 
+        const int MAX_VALUE = 2100;
+        [Required(ErrorMessage = "Please enter your name")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Please enter your birthyear")]
+        [Range(MIN_VALUE, MAX_VALUE, ErrorMessage = "Please enter your 4 digit birthyear")]
+        public int? BirthYear { get; set; }
+
+        public int? AgeThisYear()
+        {
+            int? currentYear = DateTime.Now.Year;
+            int? age = currentYear - BirthYear;
+            return age;
+        }
+    }
+}
